9730a729f99f37194fc0a13965e719ee0b017a0b
Eduard Tolosa <tolosaeduard@gmail.com>
2025-06-03 17:13:40 -0500

    (feat): also clear BASED_NO_FUZZY

--- functions/__based_reset_state.fish
+++ functions/__based_reset_state.fish
@@ -2,5 +2,6 @@ function __based_reset_state
     echo "Resetting Based state..."
     # Unset all variables related to Based state
     set -e BASED_NO_CONFIRMATION
+    set -e BASED_NO_FUZZY
     echo "Based state has been reset."
 end
